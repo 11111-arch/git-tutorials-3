e8e23ef94d46a3e63c7b9705d6d1c28b19e972f8
11111-arch <larbi.david66@gmail.com>
2025-09-10 20:33:04 +0000

    conflict 1

--- git-tutorial.js
+++ git-tutorial.js
@@ -1,4 +1,4 @@
-console.log("Welcome to the Git tutorial!");
+console.log("Welcome to the Git 1");
 console.log("This tutorial will guide you through the basics of Git.");
 console.log("Let's get started with some common commands.");
 console.log("version 3.0");
