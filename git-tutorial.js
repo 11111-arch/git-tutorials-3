91521dfbd0dfa5fca6f34835c0f18df736eac5a5
11111-arch <larbi.david66@gmail.com>
2025-09-10 19:45:51 +0000

    version 3

--- git-tutorial.js
+++ git-tutorial.js
@@ -1,4 +1,4 @@
 console.log("Welcome to the Git tutorial!");
 console.log("This tutorial will guide you through the basics of Git.");
 console.log("Let's get started with some common commands.");
-console.log("version 2.0");
+console.log("version 3.0");
